7665ac0c6dff91f21f3848e63dc19d80efc1740e
pasqualevitiello <pasqualevitiello@gmail.com>
2021-11-22 22:07:50 +0100

    1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG.md
 
+## [1.0.1] - 2021-11-22
+
+Fix dashboard icon color
+
 ## [1.0.0] - 2021-11-22
 
 First release
--- src/partials/Sidebar.vue
+++ src/partials/Sidebar.vue
@@ -62,7 +62,7 @@
                 <a class="block text-gray-200 hover:text-white truncate transition duration-150" :class="isExactActive && 'hover:text-gray-200'" :href="href" @click="navigate">
                   <div class="flex items-center">
                     <svg class="flex-shrink-0 h-6 w-6" viewBox="0 0 24 24">
-                      <path class="fill-current text-gray-400" d="M12 0C5.383 0 0 5.383 0 12s5.383 12 12 12 12-5.383 12-12S18.617 0 12 0z" />
+                      <path class="fill-current text-gray-400" :class="isExactActive && '!text-indigo-500'" d="M12 0C5.383 0 0 5.383 0 12s5.383 12 12 12 12-5.383 12-12S18.617 0 12 0z" />
                       <path class="fill-current text-gray-600" :class="isExactActive && 'text-indigo-600'" d="M12 3c-4.963 0-9 4.037-9 9s4.037 9 9 9 9-4.037 9-9-4.037-9-9-9z" />
                       <path class="fill-current text-gray-400" :class="isExactActive && 'text-indigo-200'" d="M12 15c-1.654 0-3-1.346-3-3 0-.462.113-.894.3-1.285L6 6l4.714 3.301A2.973 2.973 0 0112 9c1.654 0 3 1.346 3 3s-1.346 3-3 3z" />
                     </svg>
