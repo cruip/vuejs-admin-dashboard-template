85f3745b872b21935ecbf20973998335109a292c
pasqualevitiello <pasqualevitiello@gmail.com>
2024-07-05 09:52:13 +0200

    mc

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -60,9 +60,9 @@
             <DashboardCard08 />
             <!-- Stacked bar chart (Sales VS Refunds) -->
             <DashboardCard09 />
-            <!-- Card (Customers)  -->
+            <!-- Card (Customers) -->
             <DashboardCard10 />
-            <!-- Card (Reasons for Refunds)   -->
+            <!-- Card (Reasons for Refunds) -->
             <DashboardCard11 /> 
             <!-- Card (Recent Activity) -->
             <DashboardCard12 />
